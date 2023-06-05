4e907830acea0842fc6ea1a72243e0ee80c605b4
redaoui <redanabil14@gmail.com>
2023-06-05 18:38:52 +0100

    just started on ex00

---
+++ d01/ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 18:25:14 by rnabil            #+#    #+#             */
+/*   Updated: 2023/06/05 18:28:27 by rnabil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie
+{
+    private:
+        std::string name;
+        
+    public:
+        Zombie();
+        ~Zombie();
+        void    announce();
+};
+#endif
