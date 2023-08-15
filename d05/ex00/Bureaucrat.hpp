805fdbb1b177829c27b8be98188e2317154fdcf4
redaoui <redanabil14@gmail.com>
2023-08-15 18:32:44 +0100

    working on first exercice on d05

---
+++ d05/ex00/Bureaucrat.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 18:26:53 by rnabil            #+#    #+#             */
+/*   Updated: 2023/08/15 18:32:11 by rnabil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+    private :
+        std::string const name;
+        int               grade;
+    public:
+        /*constructors*/
+        Bureaucrat();
+        Bureaucrat(const std::string name);
+        Bureaucrat(const Bureaucrat& copy);
+        /*assignement*/
+}
+#endif
