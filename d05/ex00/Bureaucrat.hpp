683f7d33715433a64f60d3cc4d59098026a3c6b9
redaoui <redanabil14@gmail.com>
2023-08-16 08:28:47 +0100

    working on ex01 of d05

--- d05/ex00/Bureaucrat.hpp
+++ d05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 18:26:53 by rnabil            #+#    #+#             */
-/*   Updated: 2023/08/15 18:34:09 by rnabil           ###   ########.fr       */
+/*   Updated: 2023/08/16 08:26:03 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,12 @@ class Bureaucrat
         Bureaucrat(const std::string name);
         Bureaucrat(const Bureaucrat& copy);
         /*assignement*/
-}
+        Bureaucrat& operator = (const Bureaucrat& copy);
+        /*destructor*/
+        ~Bureaucrat();
+        /*methods*/
+};
 #endif
+//getters for getName() getGrade()
+//2 member functions to increment/decrement the grade (if the input is out of range throw an exception as in the constructor)
+//overload of the insertion <<
