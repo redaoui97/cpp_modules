09d099e93a6e3978f0a51eaa551569d550d52ac8
redaoui <redanabil14@gmail.com>
2023-08-24 06:27:26 +0100

    finished ex01 on d05 and working on ex02

--- d05/ex02/main.cpp
+++ d05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 18:27:58 by rnabil            #+#    #+#             */
-/*   Updated: 2023/08/24 06:14:18 by rnabil           ###   ########.fr       */
+/*   Updated: 2023/08/24 06:21:19 by rnabil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,6 @@
 
 int main(void)
 {
-    Bureaucrat A("A", 20);
-    Bureaucrat B("B", 1);
-    Form       F1("F1", 100, 5);
-    Form       F2("F2", 100, 100);
     
-    std::cout << A << std::endl;
-    std::cout << F1 << std::endl;
-    std::cout << F2 << std::endl;
-    
-    A.signForm(F1);
-    A.signForm(F2);
-    B.signForm(F2);
-    F1.beSigned(A);
     return (0);
 }
