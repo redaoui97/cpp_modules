05ad21596fa0938c0ac4442c85799803b176f804
redaoui <redanabil14@gmail.com>
2023-09-02 02:36:50 +0100

    working on last execise on d08

---
+++ d08/ex02/MutantStack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rnabil <rnabil@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/02 00:11:52 by rnabil            #+#    #+#             */
+/*   Updated: 2023/09/02 02:36:25 by rnabil           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <algorithm>
+
+class MutantStack
+{
+        
+};
+
+#endif
